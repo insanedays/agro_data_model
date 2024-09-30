069c74f1878043a16d4ef5d3f86e541551f2ce21
Amandha Nery <nery.amandha@gmail.com>
2024-09-30 20:38:09 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# Projeto de Monitoramento Agrícola
+
+## Descrição
+Este projeto visa registrar informações de manejos de insumos e leituras realizadas por sensores dentro de areas dos talhoes.
+
+# Diagrama Entidade-Relacionamento (DER)
+![alt text](diagrama_entidade_relacionamento.png)
+
+# Modelo Entidade-Relacionamento (MER) - Textual 
+## Entidades e Atributos
+
+### 1. Área
+- **id** (INTEGER) - Identificador único da área (chave primária), ele é autoincremental.
+- **id_talhao** (INTEGER) - Chave estrangeira que referencia o talhão.
+- **nome** (VARCHAR(60)) - Nome dado a aréa pelo usuário para facilitar a identificação.
+- **centroide_latitude** (DOUBLE) - Latitude do centro da área.
+- **centroide_longitude** (DOUBLE) - Longitude do centro da área.
+- **tamanho** (DECIMAL) - Tamanho da área cadastrada
+- **unidade_de_medida** (SET) - Informa se o tamanho está cadastrado em metros quadrados ou hectares.
+
+### 2. Sensor
+- **id** (INTEGER) - Identificador único do sensor (chave primária), ele é autoincremental.
+- **id_talhao** (INTEGER) - Chave estrangeira que referencia a tabela Área.
+- **nome** (VARCHAR(60)) - Nome criado para usuário para facilitar a identificação do sensor.
+- **unidade_de_medida** (SET) - Unidade de medida usada pelo sensor, cada tipo de sensor tem uma unidade padrão.
+- **tipo** (SET) - Diz o tipo do sensor. Sensor de PH, sensor de umidade, sensor de potássio, sensor de fósforo.
+- **profundidade_instalada** (DECIMAL) - Profundidade em que o sensor foi instalado.
+- **instalacao_latitude** (DOUBLE) - Latitude da instalação do sensor.
+- **instalacao_longitude** (DOUBLE) - Longitude da instalação do sensor.
+- **area_de_alcance** (DECIMAL) - Área que o sensor consegue cobrir.
+
+### 3. Leitura
+- **id** (INTEGER) - Identificador único da leitura (chave primária), ele é autoincremental.
+- **id_sensor** (INTEGER) - Chave Estrangeira que referencia a tabela Sensor.
+- **data_hora** (DATETIME) - Data e hora da leitura.
+- **valor** (INTEGER) - Valor registrado pela leitura.
+
+### 4. Talhão
+- **id** (INTEGER) - Identificador único do talhão (chave primária), ele é autoincremental.
+- **nome** (VARCHAR(60)) - Nome do talhão.
+- **hectares** (DECIMAL) - Tamanho do talhão em hectares quadrados.
+- **descricao** (VARCHAR(255)) - Descrição do talhão.
+- **centroide_latitude** (DOUBLE) - Latitude do centro do talhão.
+- **centroide_longitude** (DOUBLE) - Longitude do centro do talhão.
+
+### 5. Safra
+- **id** (INTEGER) - Identificador único da safra (chave primária), ele é autoincremental.
+- **id_talhao** (INTEGER) - Chave estrangeira que referencia o talhão.
+- **cultura** (VARCHAR(100)) - Tipo de cultura plantada.
+- **densidade_de_plantio** (DECIMAL) -  Número de plantas por hectare. 
+- **area_de_plantio** (DECIMAL) - Área plantada com a cultura.
+- **data_inicio_plantio** (DATE) - Data de início do plantio.
+- **data_fim_plantio** (DATE) - Data de fim do plantio.
+- **data_emergencia** (DATE) - Data de emergência da cultura.
+
+### 6. Ações
+- **id** (INTEGER) - Identificador único da ação (chave primária), ele é autoincremental.
+- **id_area** (INTEGER) - Chave estrangeira que referencia a tabela Área.
+- **id_produto** (INTEGER) - Chave estrangeira que referencia a tabela Produto.
+- **data_hora** (DATETIME) - Data e hora da ação.
+- **quantidade_aplicada** (DECIMAL) - Quantidade do produto aplicada.
+
+### 7. Manejo
+- **id** (INTEGER) - Identificador único do produto (chave primária).
+- **nome** (VARCHAR(60)) - Nome do produto utilizado no manejo (ex: água, fertilizante específico, produto de correção).
+- **tipo** (SET) - Informa o tipo do produto, irrigação, aplicação de fertilizantes, correção de solo.
+
+## Tabela de Cardinalidade
+
+| Relacionamento          | Entidade 1 |  Entidade 2  | Mínimo | Máximo |
+|-------------------------|------------|--------------|--------|--------|
+| Talhão ↔ Sensor         | Talhão     | Sensor       | 1:0    | 1:N    |
+| Sensor ↔ Leitura        | Sensor     | Leitura      | 1:0    | 1:N    |
+| Talhão ↔ Safra          | Talhão     | Safra        | 1:0    | 1:N    |
+| Talhão ↔ Área           | Talhão     | Área         | 1:0    | 1:N    |
+| Área ↔ Ações            | Área       | Ação         | 1:0    | N:N    |
+| Produto ↔ Ações         | Produto    | Ação         | 1:0    | 1:N    |
+
+## Explicação das Cardinalidades
+Talhao ↔ Sensor: Um talhão pode não ter não ter sensores ou ter muitos sensores. Um sensor só pode existir se estiver associado a um talhão.
+
+Sensor ↔ Leitura: Um sensor pode não ter nenhuma leitura ou pode ter muitas. Uma leitura não pode existir sem estar associada a um sensor e só pode ter um sensor. 
+
+Talhão ↔ Safra: Um talhão pode não ter nenhuma ou ter muitas safras. Uma safra só pode existir se estiver associada a um talhão e só pode estar associada a um.
+
+Talhão ↔ Área: Um talhão pode não ter nenhuma area ou ter muitas áreas, enquanto uma área deve ter (1) talhão. 
+
+Área ↔ Ações: Uma área pode não possuir nenhuma ação ou ter muitas  ações, enquanto uma ação pode acontecer em uma área (1) ou em muitas áreas.
+
+Produto ↔ Ações: Um produto deve estar associado pode não ter ações ou ter muitas  alções, enquanto uma ação só pede ter um produto (1).
---
+++ estrutura_banco.sql
@@ -0,0 +1,135 @@
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'talhao'
+-- ---
+
+DROP TABLE IF EXISTS `talhao`;
+
+CREATE TABLE `talhao` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'Identificador único ao talhão (chave primária), ele é autoincremental.',
+  `nome` VARCHAR(60) NOT NULL UNIQUE COMMENT 'Nome do talhão.',
+  `hectares` DECIMAL NOT NULL CHECK (hectares > 0) COMMENT 'Tamanho do talhão em hectares.',
+  `descricao` VARCHAR(255) NULL DEFAULT NULL COMMENT 'Descrição do talhão.',
+  `centroide_latitude` DOUBLE NOT NULL COMMENT 'Latitude do centro do talhão.',
+  `centroide_longitude` DOUBLE NOT NULL COMMENT 'Longitude do centro do talhão.',
+  PRIMARY KEY (`id`)
+) COMMENT 'Tabela de informações do talhão da fazenda';
+
+-- ---
+-- Table 'area'
+
+DROP TABLE IF EXISTS `area`;
+
+CREATE TABLE `area` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'Identificador único da área (chave primária), ele é autoincremental.',
+  `id_talhao` INTEGER NOT NULL COMMENT 'Chave estrangeira que referencia o talhão.',
+  `nome` VARCHAR(60) NOT NULL UNIQUE COMMENT 'Nome dado à área pelo usuário para facilitar a identificação.',
+  `centroide_latitude` DOUBLE NOT NULL COMMENT 'Latitude do centro da área.',
+  `centoride_longitude` DOUBLE NOT NULL COMMENT 'Longitude do centro da área.',
+  `tamanho` DECIMAL NOT NULL CHECK (tamanho > 0) COMMENT 'Tamanho da área cadastrada.',
+  `unidade_de_medida` SET('metros','hectares') NOT NULL COMMENT 'Informa se o tamanho está cadastrado em metros quadrados ou hectares.',
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (id_talhao) REFERENCES `talhao` (`id`)
+) COMMENT 'Traz detalhes sobre as áreas dentro do talhão';
+
+-- ---
+-- Table 'sensor'
+-- ---
+
+DROP TABLE IF EXISTS `sensor`;
+
+CREATE TABLE `sensor` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'Identificador único do sensor (chave primária), ele é autoincremental.',
+  `id_talhao` INTEGER NOT NULL COMMENT 'Chave estrangeira que referencia a tabela Área.',
+  `nome` VARCHAR(60) NOT NULL COMMENT 'Nome criado para o usuário para facilitar a identificação do sensor.',
+  `unidade_de_medida` SET('percentual','mg/kg','ppm') NOT NULL COMMENT 'Unidade de medida usada pelo sensor, cada tipo de sensor tem uma unidade padrão.',
+  `tipo` SET('sensor de umidade','sensor de ph','sensor de potássio','sensor de fósforo', 'escala') NOT NULL COMMENT 'Tipo do sensor: pH, umidade, potássio, fósforo.',
+  `profundidade_instalada` DECIMAL NOT NULL COMMENT 'Profundidade em que o sensor foi instalado.',
+  `instalacao_latitude` DOUBLE NOT NULL COMMENT 'Latitude da instalação do sensor.',
+  `instalacao_longitude` DOUBLE NOT NULL COMMENT 'Longitude da instalação do sensor.',
+  `area_de_alcance` DECIMAL NOT NULL CHECK (area_de_alcance > 0) COMMENT 'Área que o sensor consegue cobrir.',
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (id_talhao) REFERENCES `talhao` (`id`)
+) COMMENT 'Tabela de informações dos sensores instalados';
+
+-- ---
+-- Table 'leitura'
+-- ---
+
+DROP TABLE IF EXISTS `leitura`;
+
+CREATE TABLE `leitura` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'Identificador único da leitura (chave primária), ele é autoincremental.',
+  `id_sensor` INTEGER NOT NULL COMMENT 'Chave estrangeira que referencia a tabela Sensor.',
+  `data_hora` DATETIME NOT NULL COMMENT 'Data e hora da leitura.',
+  `valor` INTEGER NOT NULL COMMENT 'Valor registrado pela leitura.',
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (id_sensor) REFERENCES `sensor` (`id`)
+) COMMENT 'Tabela de leituras realizadas pelos sensores';
+
+-- ---
+-- Table 'acoes'
+-- ---
+
+DROP TABLE IF EXISTS `acoes`;
+
+CREATE TABLE `acoes` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'Identificador único da ação (chave primária), ele é autoincremental.',
+  `id_area` INTEGER NOT NULL COMMENT 'Chave estrangeira que referencia a tabela Área.',
+  `id_produto` INTEGER NOT NULL COMMENT 'Chave estrangeira que referencia a tabela Produto.',
+  `data_hora` DATETIME NOT NULL COMMENT 'Data e hora da ação.',
+  `quantidade_aplicada` DECIMAL NOT NULL CHECK (quantidade_aplicada > 0) COMMENT 'Quantidade do produto aplicada.',
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (id_area) REFERENCES `area` (`id`),
+  FOREIGN KEY (id_produto) REFERENCES `manejo` (`id`)
+) COMMENT 'Tabela de ações realizadas nas áreas';
+
+-- ---
+-- Table 'manejo'
+-- ---
+
+DROP TABLE IF EXISTS `manejo`;
+
+CREATE TABLE `manejo` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'Identificador único do produto (chave primária), ele é autoincremental.',
+  `nome` VARCHAR(60) NOT NULL COMMENT 'Nome do produto utilizado no manejo.',
+  `tipo` SET('irrigação', 'aplicação de fertilizantes', 'correção de solo') NOT NULL COMMENT 'Tipo do produto: irrigação, aplicação de fertilizantes, correção de solo.',
+  PRIMARY KEY (`id`)
+) COMMENT 'Tabela de manejo de insumos utilizados';
+
+-- ---
+-- Table 'safra'
+-- ---
+
+DROP TABLE IF EXISTS `safra`;
+
+CREATE TABLE `safra` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT COMMENT 'Identificador único da safra (chave primária), ele é autoincremental.',
+  `id_talhao` INTEGER NOT NULL COMMENT 'Chave estrangeira que referencia o talhão.',
+  `cultura` VARCHAR(100) NOT NULL COMMENT 'Tipo de cultura plantada.',
+  `densidade_de_plantio` DECIMAL NOT NULL CHECK (densidade_de_plantio > 0) COMMENT 'Número de plantas por hectare.',
+  `area_de_plantio` DECIMAL NOT NULL CHECK (area_de_plantio > 0) COMMENT 'Área plantada com a cultura.',
+  `data_inicio_plantio` DATE NOT NULL COMMENT 'Data de início do plantio.',
+  `data_fim_plantio` DATE NULL DEFAULT NULL COMMENT 'Data de fim do plantio.',
+  `data_emergencia` DATE NULL DEFAULT NULL COMMENT 'Data de emergência da cultura.',
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (id_talhao) REFERENCES `talhao` (`id`)
+) COMMENT 'Tabela contendo informações da safra plantada';
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `talhao` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `area` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `sensor` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `leitura` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `acoes` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `manejo` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `safra` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
